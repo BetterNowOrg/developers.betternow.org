d1211fcdcf644be1ac3a71997bfa6b692550a7f9
Matt Rohrer <matt@prognostikos.com>
2020-05-13 09:13:04 +0200

    a bunch of bullshit error suppression

--- config.rb
+++ config.rb
@@ -1,19 +1,19 @@
 # Markdown
 set :markdown_engine, :redcarpet
 set :markdown,
-    fenced_code_blocks: true,
-    smartypants: true,
-    disable_indented_code_blocks: true,
-    prettify: true,
-    tables: true,
-    with_toc_data: true,
-    no_intra_emphasis: true
+  fenced_code_blocks: true,
+  smartypants: true,
+  disable_indented_code_blocks: true,
+  prettify: true,
+  tables: true,
+  with_toc_data: true,
+  no_intra_emphasis: true
 
 # Assets
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-set :fonts_dir, 'fonts'
+set :css_dir, "stylesheets"
+set :js_dir, "javascripts"
+set :images_dir, "images"
+set :fonts_dir, "fonts"
 
 # Activate the syntax highlighter
 activate :syntax
@@ -24,9 +24,9 @@ end
 activate :sprockets
 
 activate :autoprefixer do |config|
-  config.browsers = ['last 2 version', 'Firefox ESR']
-  config.cascade  = false
-  config.inline   = true
+  config.browsers = ["last 2 version", "Firefox ESR"]
+  config.cascade = false
+  config.inline = true
 end
 
 # Github pages require relative links
@@ -45,8 +45,8 @@ configure :build do
 end
 
 activate :s3_sync do |s3_sync|
-  s3_sync.bucket = 'developer-betternow-site'
-  s3_sync.region = 'eu-west-1'
+  s3_sync.bucket = "developer-betternow-site"
+  s3_sync.region = "eu-west-1"
   s3_sync.version_bucket = true
 end
 
